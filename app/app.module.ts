5f5ee39b3919c30888d0f6e46f6c39d7f4ebe3da
aegkaluk <aegkaluk@gmail.com>
2018-06-27 16:55:20 +0700

    commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,28 +3,35 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddPage } from '../pages/add/add';
+import { ServiceProvider } from '../providers/service/service';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPage,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ServiceProvider
   ]
 })
 export class AppModule {}
--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,ModalController } from 'ionic-angular';
+import { ServiceProvider } from '../../providers/service/service';
+import { AddPage } from '../add/add';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  items:Array<any>;
+  constructor(public navCtrl: NavController,private dataService: ServiceProvider,public modalCtrl:ModalController) {
 
   }
+
+  addPage(){
+
+    let modal = this.modalCtrl.create(AddPage);
+
+    modal.onDidDismiss(student => {
+      if(student){
+        console.log("onDidDismiss: ",student);
+        //this.reviewService.createReview(review);
+        //this.reviews.push(review);        
+      }
+    })
+    modal.present();
+
+  }
+
 
 }
